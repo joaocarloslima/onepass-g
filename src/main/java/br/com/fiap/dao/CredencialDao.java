4596d5d94ed3df9272bbf112b15128c2f80c1eff
Joao <joaocarloslima@me.com>
2022-10-18 11:42:35 -0300

    feat: consulta senha

--- src/main/java/br/com/fiap/dao/CredencialDao.java
+++ src/main/java/br/com/fiap/dao/CredencialDao.java
@@ -1,6 +1,8 @@
 package br.com.fiap.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.fiap.conexao.ConnectionFactory;
 import br.com.fiap.model.Credencial;
@@ -15,6 +17,26 @@ public class CredencialDao {
         stm.setString(3, credencial.getSenha());
         stm.execute();
         conexao.close();
+    }
+
+    public List<Credencial> listar() throws SQLException{
+        var lista =  new ArrayList<Credencial>();
+
+        var conexao = ConnectionFactory.getConnection();
+        var stm  = conexao.prepareStatement("SELECT * FROM senhas ORDER BY local");
+        var rs = stm.executeQuery();
+
+        while(rs.next()){
+            var credencial = new Credencial(
+                rs.getInt("id"), 
+                rs.getString("local"), 
+                rs.getString("login"), 
+                rs.getString("senha")
+            );
+            lista.add(credencial);
+        }
+
+        return lista;
     }
     
 }
